ead7e84cd6d556a071199a13a102563f56665b1c
bratrahen <bratrahen@gmail.com>
2015-07-08 17:24:02 +0200

    remove unused methods

--- MonsterWars/src/main/java/com/monsterwars/ShipFactory.java
+++ MonsterWars/src/main/java/com/monsterwars/ShipFactory.java
@@ -1,23 +1,15 @@
 package com.monsterwars;
 
-import com.bulletphysics.collision.shapes.CollisionShape;
 
 import com.kobot.framework.entitysystem.Entity;
-import com.kobot.framework.entitysystem.components.JetEngine;
-import com.kobot.framework.entitysystem.components.RangedWeapon;
 import com.kobot.framework.entitysystem.components.Team;
-import com.kobot.framework.entitysystem.components.ai.MotherShipAi;
-import com.kobot.framework.entitysystem.components.api.Body;
 import com.kobot.framework.entitysystem.components.api.RendererComponent;
 import com.kobot.framework.entitysystem.components.factory.PrimitivesFactory;
-import com.kobot.framework.entitysystem.manager.ComponentFinder;
 import com.kobot.framework.entitysystem.manager.EntityManager;
 import com.kobot.framework.simulation.PhysicalObject;
 import com.kobot.framework.simulation.PhysicalObjectBuilder;
-import org.jetbrains.annotations.NotNull;
 
 import javax.vecmath.Vector3f;
-import java.awt.*;
 
 public abstract class ShipFactory extends PrimitivesFactory {
     private static final float PI = (float)Math.PI;
@@ -110,47 +102,6 @@ public abstract class ShipFactory extends PrimitivesFactory {
         Entity entity = entityManager.createEntity();
         entityManager.addComponentToEntity(physicalObject, entity);
         entityManager.addComponentToEntity(renderer, entity);
-
-        return entity;
-    }
-
-    @NotNull
-    protected final Vector3f calculateInertia(CollisionShape shape, float mass) {
-        Vector3f localInertia = new Vector3f(0, 0, 0);
-        boolean isDynamic = mass != 0;
-        if (isDynamic) {
-            shape.calculateLocalInertia(mass, localInertia);
-        }
-        return localInertia;
-    }
-
-    protected Entity createEntity(Body body, RendererComponent renderer) {
-        Entity entity = entityManager.createEntity();
-        entityManager.addComponentToEntity(body, entity);
-        entityManager.addComponentToEntity(renderer, entity);
-
-        return entity;
-    }
-
-    Entity createCastleWithGun(Vector3f position, long teamId) {
-        final float DAMAGE = 10.0f;
-        final float RELOAD_IN_SEC = 2.0f;
-
-        Entity entity = createStaticCube(10, Color.RED, position);
-        entityManager.addComponentToEntity(Team.getById(teamId), entity);
-        entityManager.addComponentToEntity(new RangedWeapon(DAMAGE, RELOAD_IN_SEC, this), entity);
-        entityManager.addComponentToEntity(new MotherShipAi(entityManager), entity);
-
-        return entity;
-    }
-
-    Entity createMeleeMonster(Vector3f position, long teamId) {
-        Entity entity = createDynamicSphere(1, 1, Color.BLUE, position);
-        entityManager.addComponentToEntity(Team.getById(teamId), entity);
-
-        JetEngine jetEngine = new JetEngine(new ComponentFinder(entityManager), new Vector3f(0, -10, 0));
-        entityManager.addComponentToEntity(jetEngine, entity);
-        jetEngine.setThrustPercentage(100);
 
         return entity;
     }
